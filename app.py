a3c1035ab95ad5e9d7d0487bcb0095a16e6d017f
Alyssa Harris <amh302918@gmail.com>
2022-03-14 18:06:33 -0400

    Fixed location for images

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import connect_db, db
+# from forms import LoginForm, RegisterForm, DeleteForm, FeedbackForm
+# from werkzeug.exceptions import Unauthorized
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask-feedback'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config["SECRET_KEY"] = "girlslovebeyonce"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+# connect_db(app)
+# db.create_all()
+
+@app.route('/')
+def home_page():
+    return render_template("home.html")
