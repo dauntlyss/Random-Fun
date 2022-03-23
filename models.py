3914f905ae54d26306a922e9f8a2e9abf3f77a4a
Alyssa Harris <amh302918@gmail.com>
2022-03-23 13:13:41 -0400

    Model creating import error

--- models.py
+++ models.py
@@ -1,202 +1,184 @@
-"""SQLAlchemy models for random-fun app."""
+# """SQLAlchemy models for random-fun app."""
 
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_sqlalchemy import SQLAlchemy
 
-bcrypt = Bcrypt()
-db = SQLAlchemy()
+# bcrypt = Bcrypt()
+# db = SQLAlchemy()
 
+# class Activity(db.Model):
+#     """An individual activity"""
 
-# class Follows(db.Model):
-#     """Connection of a follower <-> followed_user."""
+#     __tablename__ = 'activities'
 
-#     __tablename__ = 'follows'
-
-#     user_being_followed_id = db.Column(
+#     id = db.Column(
 #         db.Integer,
-#         db.ForeignKey('users.id', ondelete="cascade"),
 #         primary_key=True,
 #     )
 
-#     user_following_id = db.Column(
+#     name = db.Column(
+#         db.Text,
+#         nullable=False,
+#     )
+
+#     type = db.Column(
+#         db.Text,
+#         nullable=False,
+#     )
+
+#     participants = db.Column(
 #         db.Integer,
-#         db.ForeignKey('users.id', ondelete="cascade"),
+#         nullable=False,
+#     )
+
+#     accesibility = db.Column(
+#         db.Text,
+#         nullable=False,
+#     )
+
+#     price = db.Column(
+#         db.Float,
+#         nullable=False,
+#     )
+
+
+# class Recommendations(db.Model):
+#     """Mapping user likes to activities."""
+
+#     __tablename__ = 'recommendations' 
+
+#     id = db.Column(
+#         db.Integer,
+#         primary_key=True
+#     )
+
+#     user_id = db.Column(
+#         db.Integer,
+#         db.ForeignKey('users.id', ondelete='cascade')
+#     )
+
+#     activity_id = db.Column(
+#         db.Integer,
+#         db.ForeignKey('activities.id', ondelete='cascade'),
+#         unique=True
+#     )
+
+#     complete = db.Column(
+#         db.Boolean,
+#         unique=True
+#     )
+
+#     rating = db.Column(
+#         db.Integer,
+#         unique=True
+#     )
+
+#     comment = db.Column(
+#         db.Text,
+#         unique=True
+#     )
+
+
+# class User(db.Model):
+#     """User in the system."""
+
+#     __tablename__ = 'users'
+
+#     id = db.Column(
+#         db.Integer,
 #         primary_key=True,
 #     )
 
-class Activity(db.Model):
-    """An individual activity"""
+#     email = db.Column(
+#         db.Text,
+#         nullable=False,
+#         unique=True,
+#     )
 
-    __tablename__ = 'activities'
+#     username = db.Column(
+#         db.Text,
+#         nullable=False,
+#         unique=True,
+#     )
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True,
-    )
+#     image_url = db.Column(
+#         db.Text,
+#         default="/static/images/default-pic.jpg",
+#     )
 
-    name = db.Column(
-        db.Text,
-        nullable=False,
-    )
+#     header_image_url = db.Column(
+#         db.Text,
+#         default="/static/images/logo.png"
+#     )
 
-    type = db.Column(
-        db.Text,
-        nullable=False,
-    )
+#     bio = db.Column(
+#         db.Text,
+#     )
 
-    participants = db.Column(
-        db.Integer,
-        nullable=False,
-    )
+#     location = db.Column(
+#         db.Text,
+#     )
 
-    accesibility = db.Column(
-        db.Text,
-        nullable=False,
-    )
+#     password = db.Column(
+#         db.Text,
+#         nullable=False,
+#     )
 
-    price = db.Column(
-        db.Float,
-        nullable=False,
-    )
+#     activities = db.relationship(
+#         'Activity',
+#         secondary="recommendations",
+#         cascade="delete, all"
+#     )
 
-
-class Recommendations(db.Model):
-    """Mapping user likes to activities."""
-
-    __tablename__ = 'recommendations' 
-
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey('users.id', ondelete='cascade')
-    )
-
-    activity_id = db.Column(
-        db.Integer,
-        db.ForeignKey('activities.id', ondelete='cascade'),
-        unique=True
-    )
-
-    complete = db.Column(
-        db.Boolean,
-        unique=True
-    )
-
-    rating = db.Column(
-        db.Integer,
-        unique=True
-    )
-
-    comment = db.Column(
-        db.Text,
-        unique=True
-    )
+#     def __repr__(self):
+#         return f"<User #{self.id}: {self.username}, {self.email}>"
 
 
-class User(db.Model):
-    """User in the system."""
+#     @classmethod
+#     def signup(cls, username, email, password, image_url):
+#         """Sign up user.
 
-    __tablename__ = 'users'
+#         Hashes password and adds user to system.
+#         """
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True,
-    )
+#         hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
-    email = db.Column(
-        db.Text,
-        nullable=False,
-        unique=True,
-    )
+#         user = User(
+#             username=username,
+#             email=email,
+#             password=hashed_pwd,
+#             image_url=image_url,
+#         )
 
-    username = db.Column(
-        db.Text,
-        nullable=False,
-        unique=True,
-    )
+#         db.session.add(user)
+#         return user
 
-    image_url = db.Column(
-        db.Text,
-        default="/static/images/default-pic.jpg",
-    )
+#     @classmethod
+#     def authenticate(cls, username, password):
+#         """Find user with `username` and `password`.
 
-    header_image_url = db.Column(
-        db.Text,
-        default="/static/images/logo.png"
-    )
+#         This is a class method (call it on the class, not an individual user.)
+#         It searches for a user whose password hash matches this password
+#         and, if it finds such a user, returns that user object.
 
-    bio = db.Column(
-        db.Text,
-    )
+#         If can't find matching user (or if password is wrong), returns False.
+#         """
 
-    location = db.Column(
-        db.Text,
-    )
+#         user = cls.query.filter_by(username=username).first()
 
-    password = db.Column(
-        db.Text,
-        nullable=False,
-    )
+#         if user:
+#             is_auth = bcrypt.check_password_hash(user.password, password)
+#             if is_auth:
+#                 return user
 
-    activities = db.relationship(
-        'Activity',
-        secondary="recommendations",
-        cascade="delete, all"
-    )
-
-    def __repr__(self):
-        return f"<User #{self.id}: {self.username}, {self.email}>"
+#         return False
 
 
-    @classmethod
-    def signup(cls, username, email, password, image_url):
-        """Sign up user.
+# def connect_db(app):
+#     """Connect this database to provided Flask app.
 
-        Hashes password and adds user to system.
-        """
+#     You should call this in your Flask app.
+#     """
 
-        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
-
-        user = User(
-            username=username,
-            email=email,
-            password=hashed_pwd,
-            image_url=image_url,
-        )
-
-        db.session.add(user)
-        return user
-
-    @classmethod
-    def authenticate(cls, username, password):
-        """Find user with `username` and `password`.
-
-        This is a class method (call it on the class, not an individual user.)
-        It searches for a user whose password hash matches this password
-        and, if it finds such a user, returns that user object.
-
-        If can't find matching user (or if password is wrong), returns False.
-        """
-
-        user = cls.query.filter_by(username=username).first()
-
-        if user:
-            is_auth = bcrypt.check_password_hash(user.password, password)
-            if is_auth:
-                return user
-
-        return False
-
-
-def connect_db(app):
-    """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
-    """
-
-    db.app = app
-    db.init_app(app)
+#     db.app = app
+#     db.init_app(app)
