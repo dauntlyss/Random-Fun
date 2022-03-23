9f058b7f7b8908571a93ff704785aa7f481076f1
Alyssa Harris <amh302918@gmail.com>
2022-03-23 15:55:45 -0400

    routed vids to activity page

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from flask_debugtoolbar import DebugToolbarExtension
 from forms import GenerateActivityForm
 import requests
 
-from helpers import get_videos
+from helpers import determine_price, get_videos
+from googleapiclient.discovery import build
 # from models import connect_db, db
 # # from werkzeug.exceptions import Unauthorized
 
 BORED_API_BASE_URL = "https://www.boredapi.com/api/activity/"
+API_KEY = 'AIzaSyBMtqOKrUGkSTWxqeecCLN5ywVL7xkVbBE'
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 app = Flask(__name__)
 
@@ -50,25 +53,39 @@ def show_activity():
     price = data["price"]
     type = data["type"]
     
-    if price >= .8:
-        print('getting in')
-        price = '$$$$'
-    elif price >= .6:
-        price = '$$$'
-    elif price >= .4:
-        price = '$$'
-    else:
-        price = '$' 
+    price = determine_price(price)
 
     print('****************************')
     
-    response = get_videos('french bread', 5)
+    response = get_videos(activity, 5)
     print(response)
+    video1_title = response['items'][0]['snippet']['title']
+    video2_title = response['items'][1]['snippet']['title']
+    video3_title = response['items'][2]['snippet']['title']
+    video4_title = response['items'][3]['snippet']['title']
+    video5_title = response['items'][4]['snippet']['title']
+
+    video1_id = response['items'][0]['id']['videoId']
+    video2_id = response['items'][1]['id']['videoId']
+    video3_id = response['items'][2]['id']['videoId']
+    video4_id = response['items'][3]['id']['videoId']
+    video5_id = response['items'][4]['id']['videoId']
+
     activity_info = {'activity': activity, 'price': price, 'type': type}
-    return render_template('activity.html', activity_info=activity_info)
+    return render_template('activity.html', activity_info=activity_info, video1_title=video1_title, video2_title=video2_title, video3_title=video3_title, video4_title=video4_title, video5_title=video5_title, video1_id=video1_id, video2_id=video2_id, video3_id=video3_id, video4_id=video4_id, video5_id=video5_id)
 
 @app.route('/activity_videos', methods=["GET"])
 def show_videos():
     """Shows videos based on recommended activity"""
-    get_videos('playing piano', 1)
-    return render_template('videos.html')
+    # request = youtube.search().list(
+    #     part='snippet', 
+    #     maxResults=1,
+    #     q='Pickles'
+    # )
+
+    # response = request.execute()
+    # videoId = response['items'][0]['snippet']['id']['videoId']
+    response = get_videos('playing piano', 1)
+    videoId = response['items'][0]['id']['videoId']
+
+    return render_template('videos.html', videoId=videoId)
