92d3b9e540111bf0dc17b164ad1b64e665cb5d44
Alyssa Harris <amh302918@gmail.com>
2022-03-23 13:13:14 -0400

    testing youtube api connection

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db
+
 from forms import GenerateActivityForm
 import requests
-# from werkzeug.exceptions import Unauthorized
+
+from helpers import get_videos
+# from models import connect_db, db
+# # from werkzeug.exceptions import Unauthorized
 
 BORED_API_BASE_URL = "https://www.boredapi.com/api/activity/"
 
@@ -12,13 +15,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///random-fun'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config["SECRET_K        EY"] = "girlslovebeyonce"
+app.config["SECRET_KEY"] = "girlslovebeyonce"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-connect_db(app)
-db.create_all()
+# connect_db(app)
+# db.create_all()
 
 @app.route('/')
 def home_page():
@@ -39,22 +42,33 @@ def generate():
 @app.route('/activity', methods=["GET"])
 def show_activity():
     """Shows generated Activity."""
-    
-    type = "busywork"
+    form = GenerateActivityForm()
+    type = form.activity.data
     res = requests.get(f"{BORED_API_BASE_URL}", params={'type': type})
     data = res.json()
     activity = data["activity"]
     price = data["price"]
     type = data["type"]
-
+    
     if price >= .8:
+        print('getting in')
         price = '$$$$'
     elif price >= .6:
         price = '$$$'
     elif price >= .4:
         price = '$$'
     else:
-        price = '$'
-        
+        price = '$' 
+
+    print('****************************')
+    
+    response = get_videos('french bread', 5)
+    print(response)
     activity_info = {'activity': activity, 'price': price, 'type': type}
     return render_template('activity.html', activity_info=activity_info)
+
+@app.route('/activity_videos', methods=["GET"])
+def show_videos():
+    """Shows videos based on recommended activity"""
+    get_videos('playing piano', 1)
+    return render_template('videos.html')
--- helpers.py
+++ helpers.py
@@ -1,7 +1,10 @@
 import requests
+from googleapiclient.discovery import build
 
 BASE_URL="http://www.boredapi.com/api"
 AVAILABLE_TYPES=["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+API_KEY = 'AIzaSyBMtqOKrUGkSTWxqeecCLN5ywVL7xkVbBE'
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 def get_random_activity():
     response= requests.get(f"{BASE_URL}/activity/")
@@ -12,4 +15,18 @@ def get_activity_by_type(type):
         response= requests.get(f"{BASE_URL}/activity?type={type}")
         return response.json()
     return None
+    
+def get_videos(q, max_results):
+    request = youtube.search().list(
+        part='snippet', 
+        maxResults=max_results,
+        q=q
+    )
+
+    response = request.execute()
+    # print(response)
+    # print('*********')
+    # print(response['items'][0]['snippet']['title'])
+
+
 
